ec9e863e4cd1732f5b4efc1ea7587913d79f2bc3
Michael Rose <michael.rose0@gmail.com>
2025-05-24 23:53:17 -0700

    Improves 'install_and_import' to handle failed conda installs by falling back to pip

--- imports.py
+++ imports.py
@@ -1,5 +1,22 @@
 import importlib
 
+def _install_via_conda(module: str):
+    import subprocess
+    try:
+        subprocess.check_call(["conda", "install", "-y", "-c", "conda-forge", module])
+        return True
+    except subprocess.CalledProcessError:
+        print(f"  Failed to install {module} via conda.")
+        return False
+
+def _install_via_pip(module: str):
+    import subprocess
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", module])
+        return True
+    except subprocess.CalledProcessError:
+        print(f"  Failed to install {module} via pip.")
+        return False
 
 def install_and_import(module: str):
     try:
@@ -9,12 +26,16 @@ def install_and_import(module: str):
         import os
         import sys
         is_conda = os.path.exists(os.path.join(sys.base_prefix, 'conda-meta'))
-        import subprocess
+        print(f"Installing {module}. Please wait...")
+
+        install_fcn = []
         if is_conda:
-            print(f"Installing {module}. Please wait...")
-            subprocess.check_call(["conda", "install", "-y", module])
-        else:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", module])
+            install_fcn.append(_install_via_conda)
+        install_fcn.append(_install_via_pip)
+
+        for install in install_fcn:
+            if install(module):
+                break;
 
         i_mod = importlib.import_module(module)
 
